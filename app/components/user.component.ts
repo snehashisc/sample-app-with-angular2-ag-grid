37b137d49eb764ecd95bf87b485fc7f32bfe956c
Snehashis Chattopadhyay <Snehashis_C@ad.infosys.com>
2017-02-09 11:12:05 +0530

    spinner.css

--- app/components/user.component.ts
+++ app/components/user.component.ts
@@ -1,19 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import {PostsService} from '../services/posts.service';
 
 @Component({
     moduleId: module.id,
     selector: 'user',
     templateUrl: `user.component.html`,
-    providers: [PostsService],
-    inputs:['parentValue']
+    providers: [PostsService]
       
 })
 export class UserComponent{ 
 posts:Post[];
-isDataAvailed:boolean;
-parentValue: string;
-
+private gridOptions: GridOptions;
 
  constructor(private postsService: PostsService){
    
@@ -21,28 +18,115 @@ parentValue: string;
         this.posts = posts;
         this.bindGridData();
     });
-          
+        this.gridOptions = <GridOptions>{};  
+        
   }
     
     
   bindGridData() {
-   var gridDiv = document.getElementById('myGrid');
-                
-   var  gridOptions = {
+   var gridDiv = document.getElementById('myGrid');            
+   this.gridOptions = {
        columnDefs: [
-         /*{headerName: 'RequestType', field: 'RequestType'},
-        {headerName: 'Account#', field: 'Account'},
+        {headerName: 'RequestType', field: 'req'},
+        {headerName: 'RequestType', field: 'RequestType'},
+        {headerName: 'AccountNumber', field: 'AccountNumber'},
         {headerName: 'SubmittedBy', field: 'SubmittedBy'},
         {headerName: 'Created DateTime#', field: 'CreatedOn'},
-        {headerName: 'Status', field: 'Status'}*/
-        {headerName: 'Title', field: 'title'},{headerName: 'Body', field: 'body'}
-          ]
+        {headerName: 'Status', field: 'Status'},
+        {headerName: '', field: '',cellRenderer:agMenuBind,suppressFilter: true}
+        ],
+        enableSorting: true,
+        enableFilter:true,
+        onCellClicked: function(event) { onSelectionChanged(event) }, 
+        currentObj:this
+
     };  
-     new agGrid.Grid(gridDiv, gridOptions);
-     gridOptions.api.setRowData(this.posts);     
+     new agGrid.Grid(gridDiv, this.gridOptions);
+     this.gridOptions.context = {
+            myService: this;
+        } 
+     var that = this;
+   jsonload(function (data) {
+   loginType();
+  
+   setTimeout(function() {
+    if(flag == false) {
+   that.gridOptions.api.sizeColumnsToFit(); 
+			that.gridOptions.api.setRowData(data);
+            }
+            
+   },1000);
+   
+		});
  }
  
-  
+ 
+ function onSelectionChanged(e) {
+ console.log(e);
+   
+    var reqId = e.data.req;
+    if(e.event.target.className == 'glyphicon glyphicon-trash'){
+    var reqId = e.data.req;
+    
+    } else {
+    var reqId = e.data.req;
+    if(e.event.target.className == 'glyphicon glyphicon-download') {
+    e.context.myService.postsService.getDownloadData(reqId)
+        .subscribe(blob => {
+    var downloadUrl= URL.createObjectURL(blob);
+    window.open(downloadUrl);
+    });
+    }
+    
+    }
+    
+  }
+    
+
+ var flag;
+ function loginType() {
+var xhr = new XMLHttpRequest();
+		xhr.open('GET', './app/components/loginuser.json' );
+		xhr.responseType = 'json';
+		
+		xhr.onload =  function() {
+			if(this.response[0].RequestType == 'HO' || this.response[0].RequestType == 'NO' ) { 
+            flag = false;
+				
+			}	
+		};
+		
+		xhr.onerror =  function () {
+			console.log('loading data error');
+		}
+		xhr.send();
+
+}
+
+
+ function agMenuBind(params){
+ //console.log(params.context);
+return `<button class="glyphicon glyphicon-download"></button>    <span class="glyphicon glyphicon-trash"></span>`
+}
+
+  function  jsonload(callback) {
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', './app/components/db.json' );
+		xhr.responseType = 'json';
+		
+		xhr.onload =  function() {
+			if(this.status == '200') { // onload called even on 404 etc so check the status
+				callback(this.response);
+				
+			}	
+		};
+		
+		xhr.onerror =  function () {
+			console.log('loading data error');
+		}
+		xhr.send();
+	}
+ 
 }
 
 interface Post{
--- app/services/posts.service.ts
+++ app/services/posts.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
 import 'rxjs/add/operator/map';
+import {Observable} from 'rxjs/Rx';
 
 @Injectable()
 export class PostsService {
@@ -12,4 +13,33 @@ export class PostsService {
         return this.http.get('https://jsonplaceholder.typicode.com/posts')
             .map(res => res.json());
     }
+    
+    getDownloadData(value: Observable<Object[]>) {
+  return Observable.create(observer => {
+
+        let xhr = new XMLHttpRequest();
+
+        xhr.open('GET', `https://jsonplaceholder.typicode.com/posts`);
+        xhr.setRequestHeader('Content-type', 'application/json');
+        xhr.responseType='arraybuffer';
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+
+                    var contentType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+                    var blob = new Blob([xhr.response], { type: contentType });
+                    observer.next(blob);
+                    observer.complete();
+                } else {
+                    observer.error(xhr.response);
+                }
+            }
+        }
+        xhr.send();
+
+    });
+
+}
+
 }
